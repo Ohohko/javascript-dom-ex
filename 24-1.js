8fd6d6b7cd2789c0187f98841eafc1b7205b66f5
ohadk <ohadkachlon@gmail.com>
2023-01-31 11:31:18 +0200

    24-1

---
+++ 24-1.js
@@ -0,0 +1,16 @@
+JavaScript – Is square
+The following exercise contains the following subjects:
+● prototypes
+Instructions
+You are given a function, Square, that takes four parameters, a,
+b, c, d, denoting the length of the square edges.
+Using prototype properties, add a method to Square named
+isSquare that prints true if a Square object has equal edges
+and false if they are unequal.
+Here is the Square function:
+function Square(a, b, c, d){
+this.a = a;
+this.b = b;
+this.c = c;
+this.d = d;
+}
