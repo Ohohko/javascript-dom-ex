8ff4e7188eb8c769cfb9d5bc81a04d6e9531a2b2
ohadk <ohadkachlon@gmail.com>
2023-01-31 14:09:03 +0200

    24-2

---
+++ 24-2.js
@@ -0,0 +1,45 @@
+// JavaScript – Pokeman
+// The following exercise contains the following subjects:
+// ● prototypes
+// Instructions
+// You are given a function, Pokeman, that takes three
+// parameters, pokemonName, pokemonType, an array of
+// different pokemon attack methods, pokemonAttackList.
+// 1. Create three instances of the Pokemon and save them in
+// a variable.
+// 2. Using prototype properties, add the following methods:
+// ● A method called callPokemon will print the following:
+// “I choose you, <pokemon name>
+// ● • A method called attack that takes one parameter,
+// an attack number, that will print the specific attack
+// method from the pokemonAttackList array as the
+// following: “<pokemon name> used <attack method>”
+// Call each pokemon with the 2 methods you created.
+// Here is the Pokemon function:
+function Pokemon(pokemonName, pokemonType,
+pokemonAttackList){
+this.name = pokemonName;
+this.type = pokemonType;
+this.attackList = pokemonAttackList;
+}
+
+Pokemon.prototype.callPokemon = function() {
+    console.log(`I choose you, ${this.name}`);
+  };
+  
+  Pokemon.prototype.attack = function(attackNumber) {
+    console.log(`${this.name} used ${this.attackList[attackNumber]}`);
+  };
+  
+  const Pikachu = new Pokemon('Pikachu', 'Electric', ['Thunder Shock', 'Quick Attack', 'Thunderbolt']);
+  const Charizard = new Pokemon('Charizard', 'Fire/Flying', ['Flamethrower', 'Dragon Claw', 'Wing Attack']);
+  const Mewtwo = new Pokemon('Mewtwo', 'Psychic', ['Psychic', 'Shadow Ball', 'Hyper Beam']);
+  
+  Pikachu.callPokemon();
+  Pikachu.attack(2);
+  
+  Charizard.callPokemon();
+  Charizard.attack(1);
+  
+  Mewtwo.callPokemon();
+  Mewtwo.attack(0);
