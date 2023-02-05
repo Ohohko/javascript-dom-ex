2c5826c76b85cd2c21821660bef8cbd310e4f22e
ohadk <ohadkachlon@gmail.com>
2023-02-05 11:12:57 +0200

    1

---
+++ 25-1.js
@@ -0,0 +1,20 @@
+//1.this word dont get a object
+
+const hero = {
+    health: 5,
+    power: 68,
+    getStrength: function(){
+    if (this.health <= 5){
+    return this.power - 10;
+    } else return this.power;
+    }
+    }
+    function whoIsStronger(getStrength){
+    const myStrength = 82;
+    if (getStrength() < myStrength){
+    return "I am stronger";
+    } else return "You are stronger";
+    }
+    whoIsStronger(hero.getStrength);
+
+    console.log(whoIsStronger(hero.getStrength.bind(hero)));
